ac9bd0898aae687043465a87856db4006b46feff
Svapnil <Svapnil>
2019-04-06 02:44:47 -0400

    os wasn't imported

--- backend/app/map/main.py
+++ backend/app/map/main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask import abort
 import requests
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///healthdata.db'
