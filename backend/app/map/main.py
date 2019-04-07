f17f0b9bac2108a634fe7bacfeafae0ff38e6d2a
Svapnil <Svapnil>
2019-04-07 19:43:34 -0400

    added default host to app

--- backend/app/map/main.py
+++ backend/app/map/main.py
@@ -116,5 +116,6 @@ def base():
     return render_template('healthcareMap.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 33507))
-    app.run(debug=True, port=port)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
